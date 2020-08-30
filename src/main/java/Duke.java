805b87f310faca7cae79f86e32501e088d7fef01
Arindam Shiva Tripathi <arindamtripathi@gmail.com>
2020-08-30 11:18:20 +0800

    Added ability to store tasks and display them when required.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
 import java.awt.datatransfer.StringSelection;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Duke {
     static void printHello(){
-        System.out.println(" Sup! I'm Air\n" + "How can I help you out today?\n");
+        System.out.println("\t Sup! I'm Air\n" + "\tHow can I help you out today?\n");
         System.out.println("____________________________________________________________\n");
     }
     public static void main(String[] args) {
+        List Tasks = new ArrayList();
         String input;
         String logo ="   _____  .__        \n"
                      +"  /  _  \\ |__|______ \n"
@@ -13,18 +16,31 @@ public class Duke {
                      +"/    |    \\  ||  | \\/\n"
                      +"\\____|__  /__||__|   \n"
                      +"        \\/           \n";                ;
-        System.out.println("Hello from\n" + logo + "\n");
+        System.out.println("\tHello from\n" + logo + "\n");
         System.out.println("____________________________________________________________\n");
         printHello();
         Scanner scanner = new Scanner(System.in);
-        while(!(input = scanner.next()).equals("exit"))
+        while(!(input = scanner.nextLine()).equals("exit"))
         {
-            System.out.println(input+"\n");
+//            System.out.println("____________________________________________________________\n");
+            if(!input.equals("list")){
+                System.out.println("Task added:\t"+input+"\n");
+                Tasks.add(input);
+            }
+            else if(input.equals("list"))
+            {
+                int TasksLength=Tasks.size();
+                for(int i=0; i<TasksLength;i++)
+                {
+                    System.out.println(i+".\t"+Tasks.get(i)+"\n");
+                }
+            }
+
             System.out.println("____________________________________________________________\n");
         }
 
         {
-            System.out.println("Session Ending! Over and Out! \n");
+            System.out.println("\tSession Ending! Over and Out! \n");
             System.out.println("____________________________________________________________\n");
         }
 
