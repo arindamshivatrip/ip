a0621aca9b72227b7d765dc94c5531c18cd4aaa8
arindamshivatrip <47693437+arindamshivatrip@users.noreply.github.com>
2020-09-09 02:53:30 +0800

    Made some change with regards to the exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Duke {
     private static final Task[] listTasks = new Task[100];
     public static int jobCount = 0;
-    /** Function to print Logo and Hello */
+
+    /**
+     * Function to print Logo and Hello
+     */
     static void printHello() {
         String logo = "   _____  .__        \n"
                 + "  /  _  \\ |__|______ \n"
@@ -16,19 +19,26 @@ public class Duke {
         System.out.println("\tSup! I'm Air\n" + "\tHow can I help you out today?\n");
         System.out.println("____________________________________________________________\n");
     }
-    /** Function to print the list of tasks on typing list */
+
+    /**
+     * Function to print the list of tasks on typing list
+     */
     static void printTaskList(int index, Task[] listTasks) {
         for (int i = 0; i < index; i++) {
             System.out.println((i + 1) + ".  " + listTasks[i].printDetails());
         }
     }
-    /** Function to create task and add it to the listTasks */
+
+    /**
+     * Function to create task and add it to the listTasks
+     */
 
     static void newTask(Task userTask) {
         jobCount++;
         listTasks[jobCount - 1] = userTask;
     }
-    private static void gibberishError() throws NoSuchCommandException{
+
+    private static void gibberishError() throws NoSuchCommandException {
         throw new NoSuchCommandException();
     }
 
@@ -38,15 +48,14 @@ public class Duke {
         printHello();
         Scanner scanIn = new Scanner(System.in);
         int index = 0;
-        /** while loop works only till the word exit isn't typed **/
-        while (!(inputCommand = scanIn.nextLine()).equals("exit")) {
+        /** while loop works only till the word bye isn't typed **/
+        while (!(inputCommand = scanIn.nextLine()).equals("bye")) {
 //            String inputCommand = scanIn.nextLine();
             String[] splitInputs = inputCommand.split(" ", 2);
             if (inputCommand.equals("list")) {
                 printTaskList(jobCount, listTasks);
 //                System.out.println(Task.jobCount);
-            }
-            else if (inputCommand.length() >= 5) {
+            } else if (inputCommand.length() >= 4) {
                 /** Above condition used to prevent code for running for nonsensical commands or for commands where no parameters are defined*/
                 if (inputCommand.contains("done")) {
                     int taskNumber = Integer.parseInt(inputCommand.substring(5, 6));
@@ -57,45 +66,67 @@ public class Duke {
                 /** To add TODO to list */
 
                 else if ((inputCommand.contains("todo"))) {
-                    newTask(new Todo(splitInputs[1]));
-                    System.out.println("____________________________________________________________");
-                    System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
-                    System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
-                    System.out.println("____________________________________________________________");
+                    try {
+                        newTask(new Todo(splitInputs[1]));
+                        System.out.println("____________________________________________________________");
+                        System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
+                        System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
+                        System.out.println("____________________________________________________________");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("____________________________________________________________\n" +
+                                "☹ OOPS!!! The description of a " + splitInputs[0] + " cannot be empty.\n" +
+                                "____________________________________________________________\n");
+                    }
                 }
                 /** To add Deadline to list */
 
                 else if ((inputCommand.contains("deadline"))) {
-                    String[] attributeFinder = splitInputs[1].split("/by");
-                    String deadlineName = attributeFinder[0];
-                    String deadlineBy = attributeFinder[1];
-                    newTask(new Deadline(deadlineName, deadlineBy));
-                    System.out.println("____________________________________________________________");
-                    System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
-                    System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
-                    System.out.println("____________________________________________________________");
+                    try {
+                        String[] attributeFinder = splitInputs[1].split("/by");
+
+                        String deadlineName = attributeFinder[0];
+                        String deadlineBy = attributeFinder[1];
+                        newTask(new Deadline(deadlineName, deadlineBy));
+                        System.out.println("____________________________________________________________");
+                        System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
+                        System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
+                        System.out.println("____________________________________________________________");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("____________________________________________________________\n" +
+                                "☹ OOPS!!! The description of a " + splitInputs[0] + " cannot be empty.\n" +
+                                "____________________________________________________________\n");
+                    }
                 }
                 /** To add event to list */
                 else if ((inputCommand.contains("event"))) {
-                    String[] attributeFinder = splitInputs[1].split("/at");
-                    String eventName = attributeFinder[0];
-                    String eventAt = attributeFinder[1];
-                    newTask(new Events(eventName, eventAt));
-                    System.out.println("____________________________________________________________");
-                    System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
-                    System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
-                    System.out.println("____________________________________________________________");
-                }
-                else {
+                    try {
+                        String[] attributeFinder = splitInputs[1].split("/at");
+                        String eventName = attributeFinder[0];
+                        String eventAt = attributeFinder[1];
+                        newTask(new Events(eventName, eventAt));
+                        System.out.println("____________________________________________________________");
+                        System.out.println("\tTask added:\t" + listTasks[jobCount - 1].printDetails());
+                        System.out.println("\tNow you have " + jobCount + " tasks in the list.\n");
+                        System.out.println("____________________________________________________________");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("____________________________________________________________\n" +
+                                "☹ OOPS!!! The description of a " + splitInputs[0] + " cannot be empty.\n" +
+                                "____________________________________________________________\n");
+                    }
+                } else {
                     try {
                         gibberishError();
-                    }
-                    catch(NoSuchCommandException e){
+                    } catch (NoSuchCommandException e) {
                         System.out.println("____________________________________________________________\n☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n____________________________________________________________");
                     }
                 }
+            } else if (inputCommand.length() < 4) {
+                try {
+                    gibberishError();
+                } catch (NoSuchCommandException e) {
+                    System.out.println("____________________________________________________________\n☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n____________________________________________________________");
+                }
             }
-            System.out.println("____________________________________________________________");
         }
         {
             System.out.println("\tSession Ending! Over and Out! \n");
@@ -103,4 +134,3 @@ public class Duke {
         }
     }
 }
-//check
